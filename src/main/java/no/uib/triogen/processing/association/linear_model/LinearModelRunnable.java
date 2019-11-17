a3c0bf3e8e6e63ae5a7f57a1b730e6cea56797b8
mvaudel <mvaudel@gmail.com>
2019-11-17 22:37:29 +0100

    Extended the linear analysis.

--- src/main/java/no/uib/triogen/processing/association/linear_model/LinearModelRunnable.java
+++ src/main/java/no/uib/triogen/processing/association/linear_model/LinearModelRunnable.java
@@ -1,6 +1,7 @@
 package no.uib.triogen.processing.association.linear_model;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -10,7 +11,9 @@ import no.uib.triogen.io.genotypes.GenotypesProvider;
 import no.uib.triogen.io.genotypes.VariantIterator;
 import no.uib.triogen.model.family.ChildToParentMap;
 import no.uib.triogen.model.pheno.PhenotypesHandler;
+import org.apache.commons.math3.distribution.FDistribution;
 import org.apache.commons.math3.special.Beta;
+import org.apache.commons.math3.stat.regression.OLSMultipleLinearRegression;
 import org.apache.commons.math3.stat.regression.SimpleRegression;
 
 /**
@@ -87,37 +90,13 @@ public class LinearModelRunnable implements Runnable {
                 GenotypesProvider genotypesProvider = tempGenotypesProvider;
                 genotypesProvider.parse();
 
-                ArrayList<int[]> xValuesList = new ArrayList<>(4);
-
-                for (int i = 0; i < 4; i++) {
-
-                    xValuesList.add(new int[childToParentMap.children.size()]);
-
-                }
-
-                int i = 0;
-
-                for (String childId : childToParentMap.children) {
-
-                    int[] hs = genotypesProvider.getH(childToParentMap, childId);
-
-                    for (int j = 0; j < 4; j++) {
-
-                        xValuesList.get(j)[i] = hs[j];
-
-                    }
-
-                    i++;
-
-                }
-
-                IntStream.range(0, 4)
-                        .parallel()
+                phenotypesHandler.phenoMap.entrySet()
+                        .parallelStream()
                         .forEach(
-                                hI -> runLinearModel(
-                                        hI,
-                                        xValuesList.get(hI),
-                                        genotypesProvider
+                                entry -> runLinearModel(
+                                        genotypesProvider,
+                                        entry.getKey(),
+                                        entry.getValue()
                                 )
                         );
             }
@@ -131,97 +110,316 @@ public class LinearModelRunnable implements Runnable {
     }
 
     /**
-     * Runs the linear model for the given transmission model.
+     * Runs the linear models for a phenotype name.
      *
-     * @param hI the index of the h
-     * @param xValues the x values to use for regression
      * @param genotypesProvider the genotypes provider
-     */
-    public void runLinearModel(
-            int hI,
-            int[] xValues,
-            GenotypesProvider genotypesProvider
-    ) {
-
-        phenotypesHandler.phenoMap.entrySet()
-                .parallelStream()
-                .forEach(
-                        entry -> runLinearModel(
-                                hI,
-                                genotypesProvider,
-                                xValues,
-                                entry.getKey(),
-                                entry.getValue()
-                        )
-                );
-    }
-
-    /**
-     * Runs the linear model for a transmission model and a phenotype name.
-     *
-     * @param hI the index of the h
-     * @param genotypesProvider the genotypes provider
-     * @param xValues the x values to use for regression
      * @param phenoName the phenotype name
      * @param phenotypes the phenotype values
      */
-    public void runLinearModel(
-            int hI,
+    private void runLinearModel(
             GenotypesProvider genotypesProvider,
-            int[] xValues,
             String phenoName,
             double[] phenotypes
     ) {
 
-        // Histogram
-        TreeMap<Integer, Integer> hHist = new TreeMap<>();
+        // Count the number of pheno values that can be used in the regression
+        int nValidValues = (int) Arrays.stream(phenotypes)
+                .filter(
+                        y -> !Double.isNaN(y) && !Double.isInfinite(y)
+                )
+                .count();
 
-        // Select non-missing data points
-        SimpleRegression simpleRegression = new SimpleRegression();
-        int xMin = xValues[0];
-        int xMax = xValues[0];
+        // Gather the input to use for the models
+        double[] phenoY = new double[nValidValues];
+        double[][] hX = new double[nValidValues][4];
+        double[][] motherX = new double[nValidValues][2];
+        double[][] fatherX = new double[nValidValues][2];
+        double[][] childX = new double[nValidValues][2];
+        int[] hMin = new int[4];
+        Arrays.fill(hMin, Integer.MAX_VALUE);
+        int[] hMax = new int[4];
+        Arrays.fill(hMax, Integer.MIN_VALUE);
+        TreeMap<Integer, Integer>[] hHist = new TreeMap[4];
 
-        for (int i = 0; i < xValues.length; i++) {
+        int i = 0;
 
-            int x = xValues[i];
-
-            if (x > xMax) {
-
-                xMax = x;
-
-            }
-            if (x < xMin) {
-
-                xMin = x;
-
-            }
+        for (String childId : childToParentMap.children) {
 
             double y = phenotypes[i];
 
             if (!Double.isNaN(y) && !Double.isInfinite(y)) {
 
-                // Regression
-                simpleRegression.addData(x, y);
+                int[] h = genotypesProvider.getH(childToParentMap, childId);
 
-                // Histogram
-                Integer frequency = hHist.get(x);
+                for (int j = 0; j < 4; j++) {
 
-                if (frequency != null) {
+                    int hJ = h[j];
 
-                    hHist.put(x, frequency + 1);
+                    if (hJ < hMin[j]) {
 
-                } else {
+                        hMin[j] = hJ;
 
-                    hHist.put(x, 1);
+                    }
+                    if (hJ > hMax[j]) {
+
+                        hMax[j] = hJ;
+
+                    }
+
+                    TreeMap<Integer, Integer> hHistJ = hHist[j];
+                    Integer frequency = hHistJ.get(hJ);
+
+                    if (frequency != null) {
+
+                        hHistJ.put(hJ, frequency + 1);
+
+                    } else {
+
+                        hHistJ.put(hJ, 1);
+
+                    }
+                }
+
+                hX[i][0] = h[0];
+                hX[i][1] = h[2];
+                hX[i][2] = h[3];
+                hX[i][3] = h[4];
+
+                motherX[i][0] = h[1] + h[2];
+                motherX[i][1] = h[2] + h[3];
+
+                fatherX[i][0] = h[1] + h[2];
+                fatherX[i][1] = h[0] + h[1];
+
+                childX[i][0] = h[0] + h[1];
+                childX[i][1] = h[2] + h[3];
+
+                i++;
+
+            }
+        }
+
+        if (!x0 || hMax[0] - hMin[0] > 0 && hMax[1] - hMin[1] > 0 && hMax[2] - hMin[2] > 0 && hMax[3] - hMin[3] > 0) {
+
+            // Build histograms
+            String hHistograms = String.join("",
+                    "h1(",
+                    getHistogramAsString(hHist[0]),
+                    ");h2(",
+                    getHistogramAsString(hHist[1]),
+                    ");h3(",
+                    getHistogramAsString(hHist[2]),
+                    ");h4(",
+                    getHistogramAsString(hHist[3]),
+                    ")"
+            );
+
+            // Run the regressions
+            OLSMultipleLinearRegression regression = new OLSMultipleLinearRegression();
+
+            regression.newSampleData(phenoY, hX);
+            double[] hBeta = regression.estimateRegressionParameters();
+            double[] hBetaStandardErrors = regression.estimateRegressionParametersStandardErrors();
+            double[] hBetaResiduals = regression.estimateResiduals();
+            double hRSS = getRSS(hBetaResiduals);
+
+            regression.newSampleData(phenoY, motherX);
+            double[] motherBeta = regression.estimateRegressionParameters();
+            double[] motherBetaStandardErrors = regression.estimateRegressionParametersStandardErrors();
+            double[] motherBetaResiduals = regression.estimateResiduals();
+            double motherRSS = getRSS(motherBetaResiduals);
+
+            regression.newSampleData(phenoY, fatherX);
+            double[] fatherBeta = regression.estimateRegressionParameters();
+            double[] fatherBetaStandardErrors = regression.estimateRegressionParametersStandardErrors();
+            double[] fatherBetaResiduals = regression.estimateResiduals();
+            double fatherRSS = getRSS(fatherBetaResiduals);
+
+            regression.newSampleData(phenoY, childX);
+            double[] childBeta = regression.estimateRegressionParameters();
+            double[] childBetaStandardErrors = regression.estimateRegressionParametersStandardErrors();
+            double[] childBetaResiduals = regression.estimateResiduals();
+            double childRSS = getRSS(childBetaResiduals);
+            
+            // Estimate model significance
+            double significanceMother = getModelSignificance(motherRSS, 2, hRSS, 4, nValidValues);
+            double significanceFather = getModelSignificance(fatherRSS, 2, hRSS, 4, nValidValues);
+            double significanceChild = getModelSignificance(childRSS, 2, hRSS, 4, nValidValues);
+
+            
+            // Estimate slope significance
+            double[] pHBeta = IntStream.range(0, 4)
+                    .mapToDouble(
+                            j -> getBetaSignificance(hBeta[j], hBetaStandardErrors[j], nValidValues - 4)
+                    )
+                    .toArray();
+            double[] pMotherBeta = IntStream.range(0, 2)
+                    .mapToDouble(
+                            j -> getBetaSignificance(motherBeta[j], motherBetaStandardErrors[j], nValidValues - 2)
+                    )
+                    .toArray();
+            double[] pFatherBeta = IntStream.range(0, 2)
+                    .mapToDouble(
+                            j -> getBetaSignificance(fatherBeta[j], fatherBetaStandardErrors[j], nValidValues - 2)
+                    )
+                    .toArray();
+            double[] pChildBeta = IntStream.range(0, 2)
+                    .mapToDouble(
+                            j -> getBetaSignificance(childBeta[j], childBetaResiduals[j], nValidValues - 2)
+                    )
+                    .toArray();
+
+            // Export
+            String line = String.join(
+                    Utils.separator,
+                    phenoName,
+                    genotypesProvider.getVariantID(),
+                    hHistograms,
+                    Integer.toString(nValidValues),
+                    Double.toString(significanceMother),
+                    Double.toString(significanceFather),
+                    Double.toString(significanceChild),
+                    Double.toString(hBeta[0]),
+                    Double.toString(hBetaStandardErrors[0]),
+                    Double.toString(pHBeta[0]),
+                    Double.toString(hBeta[1]),
+                    Double.toString(hBetaStandardErrors[1]),
+                    Double.toString(pHBeta[1]),
+                    Double.toString(hBeta[2]),
+                    Double.toString(hBetaStandardErrors[2]),
+                    Double.toString(pHBeta[2]),
+                    Double.toString(hBeta[3]),
+                    Double.toString(hBetaStandardErrors[3]),
+                    Double.toString(pHBeta[3]),
+                    Double.toString(motherBeta[0]),
+                    Double.toString(motherBetaStandardErrors[0]),
+                    Double.toString(pMotherBeta[0]),
+                    Double.toString(motherBeta[1]),
+                    Double.toString(motherBetaStandardErrors[1]),
+                    Double.toString(pMotherBeta[1]),
+                    Double.toString(fatherBeta[0]),
+                    Double.toString(fatherBetaStandardErrors[0]),
+                    Double.toString(pFatherBeta[0]),
+                    Double.toString(fatherBeta[1]),
+                    Double.toString(fatherBetaStandardErrors[1]),
+                    Double.toString(pFatherBeta[1]),
+                    Double.toString(childBeta[0]),
+                    Double.toString(childBetaStandardErrors[0]),
+                    Double.toString(pChildBeta[0]),
+                    Double.toString(childBeta[1]),
+                    Double.toString(childBetaStandardErrors[1]),
+                    Double.toString(pChildBeta[1])
+            );
+            outputWriter.writeLine(line);
+
+        }
+    }
+
+    /**
+     * Returns the significance of increasing the complexity of a simple model,
+     * model1, with p1 parameters to a more complex model, model2, with p2
+     * parameters.
+     *
+     * @param model1RSS the residual sum of squares for model 1
+     * @param p1 the number of parameters of model 1
+     * @param model2RSS the residual sum of squares for model 2
+     * @param p2 the number of parameters of model 2
+     * @param n the number of values
+     *
+     * @return the significance
+     */
+    private double getModelSignificance(
+            double model1RSS,
+            int p1,
+            double model2RSS,
+            int p2,
+            int n
+    ) {
+
+        double numeratorDegreesOfFreedom = p2 - p1;
+        double denominatorDegreesOfFreedom = n - p2;
+        double x = ((model1RSS - model2RSS) / numeratorDegreesOfFreedom) / (model2RSS / denominatorDegreesOfFreedom);
+
+        FDistribution fDistribution = new FDistribution(numeratorDegreesOfFreedom, denominatorDegreesOfFreedom);
+
+        return fDistribution.cumulativeProbability(x);
+
+    }
+
+    /**
+     * Returns the residual sum of squares for the given residuals.
+     *
+     * @param residuals the residuals
+     *
+     * @return the residual sum of squares
+     */
+    private double getRSS(
+            double[] residuals
+    ) {
+
+        return Arrays.stream(residuals)
+                .map(
+                        x -> x * x
+                )
+                .sum();
+
+    }
+
+    /**
+     * Returns the significance of a beta.
+     *
+     * @param beta the estimate of beta
+     * @param betaSE the standard error of the estimate
+     * @param degreesOfFreedom the number of degrees of freedom
+     *
+     * @return the significance of a beta
+     */
+    private double getBetaSignificance(
+            double beta,
+            double betaSE,
+            int degreesOfFreedom
+    ) {
+
+        double p = Double.NaN;
+
+        if (degreesOfFreedom > 1 && !Double.isNaN(beta) && betaSE > 0.0) {
+
+            double x = beta / betaSE;
+
+            for (double epsilon : epsilons) {
+
+                p = x != 0.0
+                        ? Beta.regularizedBeta(
+                                degreesOfFreedom / (degreesOfFreedom + (x * x)),
+                                0.5 * degreesOfFreedom,
+                                0.5,
+                                epsilon)
+                        : 0.5;
+
+                if (p > epsilon * 16) {
+
+                    return p;
 
                 }
             }
         }
 
-        int dx = xMax - xMin;
+        return p;
 
-        // Get histogram as string
-        String hHistString = hHist.entrySet().stream()
+    }
+
+    /**
+     * Returns the histogram of h as a string.
+     *
+     * @param hHist the histogram of h
+     *
+     * @return a string for the histogram
+     */
+    private String getHistogramAsString(
+            TreeMap<Integer, Integer> hHist
+    ) {
+
+        return hHist.entrySet().stream()
                 .map(
                         entry -> String.join(
                                 ":",
@@ -232,58 +430,5 @@ public class LinearModelRunnable implements Runnable {
                 .collect(
                         Collectors.joining(",")
                 );
-
-        // Regression results
-        long n = simpleRegression.getN();
-        double slope = Double.NaN;
-        double slopeSE = Double.NaN;
-        double p = Double.NaN;
-
-        if (dx > 0) {
-
-            slope = simpleRegression.getSlope();
-            slopeSE = simpleRegression.getSlopeStdErr();
-
-            long degreesOfFreedom = n - 2;
-
-            if (degreesOfFreedom > 1 && !Double.isNaN(slope) && slopeSE > 0.0) {
-
-                double x = slope / slopeSE;
-
-                for (double epsilon : epsilons) {
-
-                    p = x != 0.0
-                            ? Beta.regularizedBeta(
-                                    degreesOfFreedom / (degreesOfFreedom + (x * x)),
-                                    0.5 * degreesOfFreedom,
-                                    0.5,
-                                    epsilon)
-                            : 0.5;
-
-                    if (p > epsilon * 16) {
-                        break;
-                    }
-                }
-            }
-        }
-
-        if (dx > 0 || !x0) {
-
-            // Export
-            String line = String.join(
-                    Utils.separator,
-                    phenoName,
-                    genotypesProvider.getVariantID(),
-                    hNames[hI],
-                    Double.toString(slope),
-                    Double.toString(slopeSE),
-                    Double.toString(simpleRegression.getRSquare()),
-                    Double.toString(p),
-                    hHistString,
-                    Long.toString(simpleRegression.getN())
-            );
-            outputWriter.writeLine(line);
-
-        }
     }
 }
