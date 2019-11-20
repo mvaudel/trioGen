ec894a2b29a6230e0f2b8f58c7a100c1aac010e5
mvaudel <mvaudel@gmail.com>
2019-11-20 13:36:18 +0100

    Fix in the docs.

--- src/R/testPhenoFile.R
+++ src/R/testPhenoFile.R
@@ -810,13 +810,13 @@ write(x = "# TrioGen test phenotypes\n", file = docsFile, append = F)
 write(x = paste0("Genotyped samples only, ADHD cases and ethnic outliers removed (N = ", nrow(phenoDF), ")\n\n"), file = docsFile, append = T)
 
 write(x = paste0("Phenotypes version V10_1.0.0-190506, standardization using [GAMLSS](https://www.gamlss.com/).\n\n"), file = docsFile, append = T)
-write(x = paste0("| Name | variable | Formula | Distribution | Normalization | n |\n"), file = docsFile, append = T)
-write(x = paste0("| --------- | ------- | ------------ | ------------- | - |\n"), file = docsFile, append = T)
-write(x = paste0("| Standardized Mother BMI | z_mother_bmi | `mother_bmi ~ fp(mother_age)` | `LOGNO` | `centiles.pred` Z-scores | ", sum(!is.na(phenoDF$z_mother_bmi)), " |\n"), file = docsFile, append = T)
-write(x = paste0("| Standardized Mother Delta BMI | z_mother_delta_bmi | `mother_delta_bmi ~ cs(z_mother_bmi)` | `NO` | `centiles.pred` Z-scores | ", sum(!is.na(phenoDF$z_mother_delta_bmi)), " |\n"), file = docsFile, append = T)
-write(x = paste0("| Standardized Father BMI | z_father_bmi | `father_bmi ~ fp(father_age)` | `LOGNO` | `centiles.pred` Z-scores | ", sum(!is.na(phenoDF$z_father_bmi)), " |\n"), file = docsFile, append = T)
-write(x = paste0("| Standardized Placenta Weight | z_placenta_weight | `placenta_weight ~ fp(pregnancy_duration)` per child sex | `BCT` | `centiles.pred` Z-scores | ", sum(!is.na(phenoDF$z_placenta_weight)), " |\n"), file = docsFile, append = T)
-write(x = paste0("| Standardized Umbilical Cord Length | z_umbilical_chord_length | `umbilical_chord_length ~ fp(pregnancy_duration)` per child sex | `BCT` | `centiles.pred` Z-scores | ", sum(!is.na(phenoDF$z_umbilical_chord_length)), " |\n"), file = docsFile, append = T)
+write(x = paste0("| Name | variable | Formula | Distribution | Normalization | n |"), file = docsFile, append = T)
+write(x = paste0("| --------- | ------- | ------------ | ------------- | - |"), file = docsFile, append = T)
+write(x = paste0("| Standardized Mother BMI | z_mother_bmi | `mother_bmi ~ fp(mother_age)` | `LOGNO` | `centiles.pred` Z-scores | ", sum(!is.na(phenoDF$z_mother_bmi)), " |"), file = docsFile, append = T)
+write(x = paste0("| Standardized Mother Delta BMI | z_mother_delta_bmi | `mother_delta_bmi ~ cs(z_mother_bmi)` | `NO` | `centiles.pred` Z-scores | ", sum(!is.na(phenoDF$z_mother_delta_bmi)), " |"), file = docsFile, append = T)
+write(x = paste0("| Standardized Father BMI | z_father_bmi | `father_bmi ~ fp(father_age)` | `LOGNO` | `centiles.pred` Z-scores | ", sum(!is.na(phenoDF$z_father_bmi)), " |"), file = docsFile, append = T)
+write(x = paste0("| Standardized Placenta Weight | z_placenta_weight | `placenta_weight ~ fp(pregnancy_duration)` per child sex | `BCT` | `centiles.pred` Z-scores | ", sum(!is.na(phenoDF$z_placenta_weight)), " |"), file = docsFile, append = T)
+write(x = paste0("| Standardized Umbilical Cord Length | z_umbilical_chord_length | `umbilical_chord_length ~ fp(pregnancy_duration)` per child sex | `BCT` | `centiles.pred` Z-scores | ", sum(!is.na(phenoDF$z_umbilical_chord_length)), " |"), file = docsFile, append = T)
 
 for (ageI in 0:11) {
     
@@ -830,9 +830,10 @@ for (ageI in 0:11) {
     }
     formula <- paste0(variable, sigmaFormula)
     
-    write(x = paste0("| ", phenoName, " | ", variable, " | `", formula, "` per child sex | `LOGNO` | `centiles.pred` Z-scores | ", sum(!is.na(phenoDF[[variable]])), " |\n"), file = docsFile, append = T)
+    write(x = paste0("| ", phenoName, " | ", variable, " | `", formula, "` per child sex | `LOGNO` | `centiles.pred` Z-scores | ", sum(!is.na(phenoDF[[variable]])), " |"), file = docsFile, append = T)
     
 }
+write(x = "<br>", file = docsFile, append = T)
 
 
 pheno1 <- "mother_age"
