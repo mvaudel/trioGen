a276bcb0cab43e8505504d4d0d601c18bc1827d5
mvaudel <mvaudel@gmail.com>
2020-04-10 15:40:55 +0200

    Update in the moba ld script.

--- scripts/test_moba_ld.sh
+++ scripts/test_moba_ld.sh
@@ -13,11 +13,11 @@ outputFolder=~/mnt/work/marc/moba/test_TrioGen/ld
 # Variables
 
 # Commands
-for chr in 22
+for chr in 7 10 9 6 5 3 2 1
 do
 
     echo "Processing chromosome $chr"
 
-    java -Xmx100G -cp bin/triogen-0.4.0-beta/triogen-0.4.0-beta.jar no.uib.triogen.cmd.ld.LdMatrix -g $vcfFolder/$chr.vcf.gz -gf 1 -f $trioFile -o $outputFolder/chr_$chr -nv 32 -t
+    java -Xmx100G -cp bin/triogen-0.4.0-beta/triogen-0.4.0-beta.jar no.uib.triogen.cmd.ld.LdMatrix -g $vcfFolder/$chr.vcf.gz -gf 1 -f $trioFile -o $outputFolder/chr_$chr -nv 32
 
 done
