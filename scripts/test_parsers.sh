3d872823c163287bdb792e780d0394bea0931618
mvaudel <mvaudel@gmail.com>
2020-07-27 10:03:34 +0200

    Minor fix.

--- scripts/test_parsers.sh
+++ scripts/test_parsers.sh
@@ -4,6 +4,6 @@
 # This script checks that vcf file parsers return the same results.
 ##
 
-java -cp bin/triogen-0.4.0-beta/triogen-0.4.0-beta.jar no.uib.triogen.test_scripts \
+java -cp bin/triogen-0.4.0-beta/triogen-0.4.0-beta.jar no.uib.triogen.test_scripts.TestVcfParsers \
              /mnt/archive/MOBAGENETICS/genotypes-base/imputed/all/vcf/1.vcf.gz \
              /mnt/work/marc/moba/run/triogen/pheno/trio
