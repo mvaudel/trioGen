cfa661744c8eeae96b86804083cde2de1da5a452
mvaudel <mvaudel@gmail.com>
2020-07-27 12:34:44 +0200

    Minor fix.

--- scripts/test_mendelian_errors.sh
+++ scripts/test_mendelian_errors.sh
@@ -8,6 +8,6 @@ java -cp bin/triogen-0.4.0-beta/triogen-0.4.0-beta.jar no.uib.triogen.cmd.mendel
              -g /mnt/archive/MOBAGENETICS/genotypes-base/imputed/all/vcf/22.vcf.gz \
              -gf 1 \
              -f /mnt/work/marc/moba/run/triogen/pheno/trio \
-             -maf 0.005 \ 
+             -maf 0.005 \
              -o /mnt/work/marc/moba/run/triogen/m_err/chr22 \
              -nv 42
