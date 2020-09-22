a02b70f058b5b9bf9b9eb3c9cc318c0511a1162a
mvaudel <mvaudel@gmail.com>
2020-09-22 09:20:53 +0200

    Added a script to test the ld pruning.

---
+++ scripts/test_pruning.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+##
+# This script makes a test run on the moba data.
+##
+
+java -Xmx5G -cp bin/triogen-0.5.0-beta/triogen-0.5.0-beta.jar no.uib.triogen.cmd.ld_pruning.LdPruningOptions \
+             -res /mnt/work/marc/moba/run/bolt/bolt_output/childGeno_z_bmi3-stats-bgen.gz \
+			 -l /mnt/work/marc/moba/run/triogen/ld \
+			 -id SNP \
+			 -pn P_BOLT_LMM \
+			 -cn CHR \
+			 -o /mnt/work/marc/moba/run/bolt/bolt_output/pruned/childGeno_z_bmi3.gz
