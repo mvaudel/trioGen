5aa05c1d7f0d397e1cdbc1bbcc135ba9d3e24ce6
mvaudel <mvaudel@gmail.com>
2020-09-22 12:20:37 +0200

    Fix.

--- scripts/test_pruning.sh
+++ scripts/test_pruning.sh
@@ -6,7 +6,7 @@
 
 java -Xmx5G -cp bin/triogen-0.5.0-beta/triogen-0.5.0-beta.jar no.uib.triogen.cmd.ld_pruning.LdPruning \
              -res /mnt/work/marc/moba/run/bolt/bolt_output/childGeno_z_bmi3-stats-bgen.gz \
-			 -l /mnt/work/marc/moba/run/triogen/ld \
+			 -l /mnt/work/marc/moba/run/triogen/ld/dos_chr_{contig}.tld \
 			 -id SNP \
 			 -pn P_BOLT_LMM \
 			 -cn CHR \
