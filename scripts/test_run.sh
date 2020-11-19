1a3a9a7ee2e7d13a436e277ba2aff8c7be7ec359
mvaudel <mvaudel@gmail.com>
2020-11-19 09:48:23 +0100

    Debug

--- scripts/test_run.sh
+++ scripts/test_run.sh
@@ -5,16 +5,14 @@
 ##
 
 java -Xmx32G -cp bin/triogen-0.5.0-beta/triogen-0.5.0-beta.jar no.uib.triogen.cmd.association.LinearModel \
-             -g /mnt/archive/MOBAGENETICS/genotypes-base/imputed/all/vcf/23.vcf.gz \
-             -gf 1 \
-             -vi /mnt/work/marc/moba/mobaRun/resources/triogen/targets/targets_h2020 \
+             -g /mnt/archive/MOBAGENETICS/genotypes-base/imputed/all/vcf/2.vcf.gz \
              -f /mnt/work/marc/moba/run/triogen/pheno/trio \
              -p /mnt/work/marc/moba/run/triogen/pheno/trio_pheno \
-             -pn "z_bmi0" \
+             -pn "z_placenta_weight" \
              -cg "child_harvest,child_Rotterdam1,child_Rotterdam2,child_NormentMay16,child_NormentFeb18,child_Ted,PC1,PC2,PC3,PC4,PC5,PC6,PC7,PC8,PC9,PC10" \
              -cs /mnt/work/marc/moba/run/triogen/pheno/standardization_details.json \
              -maf 0.005 \
              -dos \
-             -o /mnt/work/marc/moba/run/triogen/debug/test_X.lm_target.gz \
+             -o /mnt/work/marc/moba/run/triogen/lm_output/gwas/z_placenta_weight/2.z_placenta_weight \
              -nv 12 \
              -vl
